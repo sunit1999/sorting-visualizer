38acf358e164fa854ec58ff4a3133f604cf641fa
sunit1999 <sunit1999@users.noreply.github.com>
2022-03-31 12:53:17 +0000

    deploy: 020fa1231641585201819215f8e0cee99c5bc28a

--- src/toDispatch.js
+++
@@ -1,7 +0,0 @@
-export const toDispatch = (dispatch, toDispatchArray, animationSpeed) => {
-  for (let i = 0; i < toDispatchArray.length; i++) {
-    setTimeout(() => {
-      dispatch(toDispatchArray[i]);
-    }, i * animationSpeed);
-  }
-};
